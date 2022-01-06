3e93c96ee458bad76a2adf8422d8291729006e8f
AnandShetler <anandshetler@gmail.com>
2022-01-06 14:52:34 -0500

    garbage

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask.helpers import send_file, send_from_directory
+from flask import Flask, render_template, request
+from flask.helpers import send_file
 from midiutil import MIDIFile
 
 app = Flask(__name__)
@@ -9,7 +9,8 @@ def home():
     return render_template("index.html")
 
 @app.route("/create-midi")
-def create_midi():
+# input format: 1D?? list of ALL tracks containing note tuples of (fruit, x, y)??
+def create_midi(tracks):
     degrees  = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
     track    = 0
     channel  = 0
@@ -21,6 +22,16 @@ def create_midi():
     MyMIDI.addTempo(track, time, tempo)
     for i, pitch in enumerate(degrees):
         MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+
+    tracks = request.json
+    midiFile = MIDIFile(len(tracks))
+    trackNum = 0
+    for track in tracks:
+        midiFile.addTempo(trackNum, 0, 60)
+        for note in track:
+            midiFile.addNote(trackNum, channel, note[0], note[1], duration, volume)
+        trackNum += 1
+
 
     with open("major-scale.mid", "wb") as output_file:
         MyMIDI.writeFile(output_file)
