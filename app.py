30b0f1680de132f7fa720e694c4bd295b7d33ae7
AnandShetler <anandshetler@gmail.com>
2022-01-07 20:16:52 -0500

    Suuuper basic functionality

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
 from flask.helpers import send_file
 from midiutil import MIDIFile
+import json
 
 app = Flask(__name__)
+
+fruitNotes = {"orange":60, "apple":72}
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/create-midi")
-# input format: 1D?? list of ALL tracks containing note tuples of (fruit, x, y)??
-def create_midi(tracks):
+@app.route("/create-midi", methods=["GET","POST"])
+def create_midi():
     degrees  = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
     track    = 0
     channel  = 0
@@ -18,21 +20,24 @@ def create_midi(tracks):
     duration = 1    # In beats
     tempo    = 60   # In BPM
     volume   = 100
-    MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created automatically)
-    MyMIDI.addTempo(track, time, tempo)
-    for i, pitch in enumerate(degrees):
-        MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+    # MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created automatically)
+    # MyMIDI.addTempo(track, time, tempo)
+    # for i, pitch in enumerate(degrees):
+    #     MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+    # with open("major-scale.mid", "wb") as output_file:
+    #     MyMIDI.writeFile(output_file)
+    # return send_file("major-scale.mid", as_attachment=True, mimetype="audio/midi")
 
-    tracks = request.json
+    tracks = json.loads(request.args.get("tracks"))
     midiFile = MIDIFile(len(tracks))
     trackNum = 0
     for track in tracks:
         midiFile.addTempo(trackNum, 0, 60)
         for note in track:
-            midiFile.addNote(trackNum, channel, note[0], note[1], duration, volume)
+            midiFile.addNote(trackNum, channel, fruitNotes[note[0]], note[1], duration, volume)
         trackNum += 1
 
 
-    with open("major-scale.mid", "wb") as output_file:
-        MyMIDI.writeFile(output_file)
-    return send_file("major-scale.mid", as_attachment=True, mimetype="audio/midi")
+    with open("your-midi.mid", "wb") as output_file:
+        midiFile.writeFile(output_file)
+    return send_file("your-midi.mid", as_attachment=True, mimetype="audio/midi")
